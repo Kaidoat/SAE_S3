fcce6ba2afe3d1b7965120cd60ab65880025b278
chaima-k <chaima.kherbach@etu.u-paris.fr>
2026-01-03 23:54:44 +0100

    connexion

--- back/login-interne.php
+++ back/login-interne.php
@@ -1,23 +1,26 @@
 <?php
-session_start();
 require_once '../config/db.php';
+session_start();
 
-$login = $_POST['login'] ?? '';
-$password = $_POST['password'] ?? '';
+$login = $_POST['login'];
+$password = $_POST['password'];
 
-$stmt = $pdo->prepare("SELECT * FROM utilisateur WHERE login = ?");
+$stmt = $pdo->prepare("
+    SELECT * FROM utilisateur 
+    WHERE login = ?
+");
 $stmt->execute([$login]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// mot de passe EN CLAIR (SAE)
-if ($user && $password === $user['password_hash']) {
-    $_SESSION['user'] = $user['login'];
-    $_SESSION['role'] = $user['role'];
-
-    header("Location: ../espace-interne.php");
+// Vérification simple
+if (!$user || $user['password_hash'] !== $password) {
+    header("Location: ../login-interne.php?error=1");
     exit;
 }
 
-// sinon erreur
-header("Location: ../login-interne.php?error=1");
+// Connexion OK
+$_SESSION['user'] = $user['login'];
+$_SESSION['role'] = $user['role'];
+
+header("Location: ../espace-interne.php");
 exit;
